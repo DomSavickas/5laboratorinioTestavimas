83ebc315dba4f166fbb54201e917a26a5a6903d2
Dominykas <dominykas.savickas@knf.stud.vu.lt>
2020-12-07 23:46:19 +0200

    Initial commit (Unit tests complete)

--- app/src/test/java/com/example/a5laboratorinis/ExampleUnitTest.java
+++ app/src/test/java/com/example/a5laboratorinis/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.example.a5laboratorinis;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +13,30 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void Given_InvalidString_When_getRateFromECBIsCalled_Then_ReturnErrorMessage() throws IOException {
+        final String givenString = "USDA";
+
+        final String expectedResult = "There is no information about this currency";
+        final String actualResult = DataManager.getRateFromECB(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void Given_SpaceString_When_getRateFromECBIsCalled_Then_ReturnErrorMessage() throws IOException {
+        final String givenString = "";
+
+        final String expectedResult = "There is no information about this currency";
+        final String actualResult = DataManager.getRateFromECB(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void Given_Null_When_getRateFromECBIsCalled_Then_ReturnErrorMessage() throws IOException {
+        final String givenString = null;
+
+        final String expectedResult = "There is no information about this currency";
+        final String actualResult = DataManager.getRateFromECB(givenString);
+
+        assertEquals(expectedResult, actualResult);
     }
 }
